1c1330ac29b9ce70005ef57edb505a9f06e98cb0
Omar Faruk <ofaruk0169@gmail.com>
2020-03-24 17:10:52 +0000

    remove elements via the parent node to child node remove feature

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const bookList = document.querySelector('#book-list');
 
-console.log('the parent node is:', bookList.parentNode);
-
 bookList.previousElementSibling.querySelector('p').innerHTML += '<br />The coolest bookshelf'
+
+var btns = document.querySelectorAll('#book-list .delete');
+
+Array.from(btns).forEach(function(btn) {
+    btn.addEventListener('click', function(e) {
+
+        const li = e.target.parentElement;
+
+        //go up one node in order to remove the child (which is the element we're looking at)
+        li.parentNode.removeChild(li);
+
+    });
+})
