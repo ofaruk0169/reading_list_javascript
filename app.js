cad08a1ad7925f9f2e7650ab0381c47a73769a66
Omar Faruk <ofaruk0169@gmail.com>
2020-03-24 16:52:41 +0000

    added some innerhtml

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const search = document.getElementById('search-books');
-const bookList = document.getElementById('book-list');
+const bookList = document.querySelector('#book-list');
 
-console.log(search, bookList);
+console.log('the parent node is:', bookList.parentNode);
+
+bookList.previousElementSibling.querySelector('p').innerHTML += '<br />The coolest bookshelf'
