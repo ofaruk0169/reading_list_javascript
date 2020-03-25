a716346fb37469d61fdcfe8369dd2701cf508e43
Omar Faruk <ofaruk0169@gmail.com>
2020-03-25 10:46:54 +0000

    the relevant classes have been added to both the book list and delete button

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const list = document.querySelector('#book-list ul');
 
+//delete books
 list.addEventListener('click', function(e) {
+    
     //when we click on the ul, if the target has the class name of 'delete' then we do something
-
     if(e.target.className == 'delete') {
 
         //the button that we clicked's parent element
@@ -11,3 +12,34 @@ list.addEventListener('click', function(e) {
         list.removeChild(li);
     }
 });
+
+//add book list
+
+const addForm = document.forms['add-book'];
+
+addForm.addEventListener('submit', function(e) {
+
+    e.preventDefault();
+    //get whatever value with the 'value' function
+    const value = addForm.querySelector('input[type="text"]').value;
+    
+    //create elements
+    const li = document.createElement('li');
+    const bookName = document.createElement('span');
+    const deleteBtn = document.createElement('span');
+
+    //add content
+    deleteBtn.textContent = 'delete';
+    bookName.textContent = value;
+
+    //add classes
+    bookName.classList.add('name');
+    deleteBtn.classList.add('delete');
+
+    //append to document
+    //this appends each of the span tags to the li tag. Which is the general strucutre of the book list. The li tag needs appending to the ul tag also
+    li.appendChild(bookName);
+    li.appendChild(deleteBtn);
+    //the ul was defined earlier and we need to append the created li element
+    list.appendChild(li);
+})
