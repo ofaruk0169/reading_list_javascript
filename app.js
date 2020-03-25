801755928f7112ed9a6c48c150fd31a44a30d41f
Omar Faruk <ofaruk0169@gmail.com>
2020-03-25 12:48:30 +0000

    the search filter works now

--- app.js
+++ app.js
@@ -59,3 +59,35 @@ hideBox.addEventListener('change', function(e) {
     }
 
 })
+
+//search filter for specific books
+
+//first we must grab the search bar and query select the input field
+const searchBar = document.forms['search-books'].querySelector("input");
+//next we must add an eventListener which is the keyup event. Then to this we get the
+//target value with the appropriate method and convert it to lowercase and store the result in "term"
+searchBar.addEventListener("keyup", function(e) {
+    const term = e.target.value.toLowerCase();
+    //next grab all "li" tags. The "ul" tag was defined earlier, so we can use a getElemenht method to get the li tag
+    const books = list.getElementsByTagName("li"); 
+    
+    //next we need to cycle through each of the "li" tags
+    Array.from(books).forEach(function(book) {
+
+        //within the loop we grab the first child element (which is the span)
+        //within this span, we get the text content with the textContent method
+        const title = book.firstElementChild.textContent;
+
+        //Now we have to check if the "term" variable is within the "title" with a if check
+        //indexOf will get the position of term in the title
+
+        //if indexOf finds no value, it returns -1. If that is the case we display display as "none"
+        if(title.toLowerCase().indexOf(term) != -1) {
+            book.style.display = "block";
+        } else {
+            book.style.display = "none";
+        }
+
+    })
+})
+
